c09f70ca323ba45bbd41a9905c1a349bd43938d6
Aleks <crni.aki@gmail.com>
2025-03-22 09:46:02 +0100

    Update content_short_engine.py

--- shortGPT/engine/content_short_engine.py
+++ shortGPT/engine/content_short_engine.py
@@ -160,8 +160,8 @@ class ContentShortEngine(AbstractContentEngine):
         if not os.path.exists('videos/'):
             os.makedirs('videos')
         #self._db_yt_title, self._db_yt_description = gpt_yt.generate_title_description_dict(self._db_script)
-        self._db_yt_title, self._db_yt_description = ["Interesting facts #facts", "Intersting facts about ... "]
-
+        self._db_yt_title, self._db_yt_description = {"title": "nteresting facts", "description":"nteresting facts"}
+      
         now = datetime.datetime.now()
         date_str = now.strftime("%Y-%m-%d_%H-%M-%S")
         newFileName = f"videos/{date_str} - " + \
