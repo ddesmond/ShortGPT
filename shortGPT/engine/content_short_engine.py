d7d69db273fe46360c17c01c7ab7586597e8fecf
Aleks <crni.aki@gmail.com>
2025-03-24 07:35:30 +0100

    Update content_short_engine.py

--- shortGPT/engine/content_short_engine.py
+++ shortGPT/engine/content_short_engine.py
@@ -174,3 +174,4 @@ class ContentShortEngine(AbstractContentEngine):
                 f"---Youtube title---\n{self._db_yt_title}\n---Youtube description---\n{self._db_yt_description}")
         self._db_video_path = newFileName+".mp4"
         self._db_ready_to_upload = True
+        print("Adding metadata done.")
