c3cdcf6bdd70c0e9c7938b4df0fc08003bf54ddd
Aleks <crni.aki@gmail.com>
2025-03-24 07:12:30 +0100

    Update content_short_engine.py

--- shortGPT/engine/content_short_engine.py
+++ shortGPT/engine/content_short_engine.py
@@ -77,9 +77,10 @@ class ContentShortEngine(AbstractContentEngine):
 
     def _generateImageSearchTerms(self):
         self.verifyParameters(captionsTimed=self._db_timed_captions)
-        if self._db_num_images:
-            self._db_timed_image_searches = gpt_editing.getImageQueryPairs(
-                self._db_timed_captions, n=self._db_num_images)
+        return
+#        if self._db_num_images:
+#            self._db_timed_image_searches = gpt_editing.getImageQueryPairs(
+#                self._db_timed_captions, n=self._db_num_images)
 
     def _generateImageUrls(self):
         if self._db_timed_image_searches:
